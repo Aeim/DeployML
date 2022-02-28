8bf930d5ae5b93ca65201d485f67c288bfcab48e
Aeim <thitiwataeim@gmail.com>
2022-02-28 16:34:19 +0900

    add mark model

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 from flask import Flask, render_template, request
+import marks as m
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template("index.html")
-
-@app.route("/sub", methods = ["POST"])
-def submit():
-    # HTML -> .py
+@app.route("/", methods = ['GET','POST'])
+def marks():
     if request.method == "POST":
-        name = request.form["username"]
+        hrs = request.form['hrs']
+        marks_pred = m.marks_prediction(hrs)
+    return render_template("index.html", my_marks=marks_pred)
 
-    # .py -> HTML
-    return render_template("sub.html", n = name)
+# @app.route("/sub", methods = ["POST"])
+# def submit():
+#     # HTML -> .py
+#     if request.method == "POST":
+#         name = request.form["username"]
+
+#     # .py -> HTML
+#     return render_template("sub.html", n = name)
 
 if __name__ == "__main__":
     app.run(debug=True)
